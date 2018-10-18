7ce9722dfc135b8fe24f03079db317fa0427ac19
Ryan <ryanthomas1998@gmail.com>
2018-10-18 18:44:44 -0400

    Added the iOS project:

---
+++ iOS/ECOS/ECOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ECOS
+//
+//  Created by Ryan Williamson on 10/18/18.
+//  Copyright © 2018 Ryan Williamson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
